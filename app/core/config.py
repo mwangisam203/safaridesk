8cae1aa832c6dab260d1a849f421102a5f6e4dd2
mwangisam203 <mwangisam203@gmail.com>
2026-06-09 14:07:22 -0400

    feat(config): add frontend application URL

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "SafariDesk"
     APP_ENV: str = "development"
     APP_BASE_URL: str = "http://localhost:8000"
+    FRONTEND_URL: str = "http://localhost:3000"
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
